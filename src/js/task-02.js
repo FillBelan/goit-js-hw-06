00aa1f964a70ddeee19ba88a13eb6057527f09da
FilBelan <filbelan@gmail.com>
2022-11-10 17:36:20 +0200

    task 2 done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const vegetables = document.querySelector('#ingredients');
+
+const elements = ingredients.map((options, index) => {
+    const liElem = document.createElement('li');
+  liElem.textContent = ingredients[index];
+  liElem.classList.add('item');
+
+  return (liElem);
+});
+
+console.log(elements);
+vegetables.append(...elements);
