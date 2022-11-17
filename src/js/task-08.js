0786ab402bb5c3721c612ebb66b2270eb574bb20
FilBelan <filbelan@gmail.com>
2022-11-17 17:02:55 +0200

    task 8 Done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
 
+const autorizationForm = document.querySelector(".login-form");
+
+autorizationForm.addEventListener('submit', checkInput);
+function checkInput(event) {
+    event.preventDefault();
+    const form = event.currentTarget.elements;
+    const email = form.email.value;
+    const password = form.password.value;
+
+
+    if (email.value || password.value === '') {
+        alert ('Все поля должны быть заполнены!');
+return;
+    }
+    const user = { email, password };
+
+    console.log(user);
+    autorizationForm.reset();
+};
+
+
+
+
+
