911fe18207e17ab9c7c5bf128447e9d0a6cc4d17
FilBelan <filbelan@gmail.com>
2022-11-17 18:27:51 +0200

    task 9 Done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const spanColor = document.querySelector('.color');
+const bgColor = document.querySelector('.change-color');
+bgColor.addEventListener('click', e => {
+  body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+});
